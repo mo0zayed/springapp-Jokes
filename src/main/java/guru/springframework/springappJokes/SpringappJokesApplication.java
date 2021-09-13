c16d6dfef3812a64980cc15629e148738b9e63c1
mostafa zayed <mostafa.zayed97@gmail.com>
2021-09-13 12:51:49 +0200

    first

---
+++ src/main/java/guru/springframework/springappJokes/SpringappJokesApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.springappJokes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringappJokesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringappJokesApplication.class, args);
+	}
+
+}
